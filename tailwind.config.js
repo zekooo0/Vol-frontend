1f9eb1124e751a80fb927624f7dc8c753e603192
Ahmed Zakaria <ahmedzakria102@gmail.com>
2024-03-26 09:46:52 +0200

    fix homepage image in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['bg-home-image'],
+    },
+  },
   theme: {
     container: {
       center: true,
