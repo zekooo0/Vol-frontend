7ec36f7e38353d9179aefe41041ea57a59f6414a
Ahmed Zakaria <ahmedzakria102@gmail.com>
2024-03-28 09:06:30 +0200

    responsive opportunity slider

--- src/components/OpportunitiesSlider.jsx
+++ src/components/OpportunitiesSlider.jsx
@@ -43,20 +43,20 @@ const OpportunitiesSlider = () => {
             {data.map((opp, i) => (
               <SwiperSlide key={i}>
                 <div className="flex items-center gap-5">
-                  <div className="w-1/3">
+                  <div className="md:w-1/3">
                     <h1 className="pb-2 text-lg font-medium">{opp.title}</h1>
                     <p>{opp.desc}</p>
                   </div>
-                  <div className="h-60 w-[1px] bg-gray-300"></div>
-                  <div className="w-1/3">
+                  <div className="h-60 w-[1px] bg-gray-300 hidden md:block"></div>
+                  <div className="md:block hidden w-1/3">
                     <p>{`${opp.country}, ${opp.city}`}</p>
                     <p>{opp.type}</p>
                     <p>{` عدد المتطوعين المطلوبين: ${opp.requestedVolunteersCount}`}</p>
                     <p>{` عدد المتقدمين : ${opp.appliedVolunteers.length}`}</p>
                   </div>
-                  <div className="h-60 w-[1px] bg-gray-300"></div>
+                  <div className="h-60 w-[1px] bg-gray-300 md:block hidden"></div>
 
-                  <div className="w-1/3">
+                  <div className="md:block hidden w-1/3">
                     <img
                       src={opp.photo}
                       className="aspect-square h-60 w-auto bg-cover rounded-md"
