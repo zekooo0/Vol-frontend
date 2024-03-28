004cac6d3a88abd8407b07ce4d634b69b19d8963
Soha Ali <sohaali710@gmail.com>
2024-03-28 23:58:05 +0200

    Edit in links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,43 @@
-import { Link } from 'react-router-dom';
-import useLogout from '../hooks/useLogout';
-import Logo from '../assets/logo2_without_bg.png';
+import { Link } from "react-router-dom";
+import useLogout from "../hooks/useLogout";
+import Logo from "../assets/logo2_without_bg.png";
 
 const Header = () => {
   const { logout, isAuthenticated } = useLogout();
 
   return (
-    <div className=" flex flex-row items-center w-full h-20 text-black">
+    <div className=" flex flex-row items-center w-full h-20 text-black shadow">
       <header className="container flex items-center justify-between">
         <div className=" flex items-center gap-10">
-          <div>
+          <Link to="/">
             <img src={Logo} alt="logo" className="w-40" />
-          </div>
+          </Link>
           <ul className="md:flex hidden gap-10 text-lg font-medium">
             <li>
-              <a href="#landing">الصفحة الرئيسية</a>
+              <a href="/">الصفحة الرئيسية</a>
             </li>
 
             <li>
-              <Link to="">الفرص التطوعية</Link>
+              <Link to="/opportunities">الفرص التطوعية</Link>
             </li>
             <li>
-              <a href="#about">من نحن</a>
+              <a href="/about">من نحن</a>
             </li>
           </ul>
         </div>
         <div>
           {isAuthenticated ? (
-            <button onClick={logout} className="text-neutral-200 rounded-3xl  p-3 bg-[#003478]">
+            <button
+              onClick={logout}
+              className="text-neutral-200 rounded-3xl  p-3 bg-[#003478] hover:bg-[#00c2cd]"
+            >
               تسجيل خروج
             </button>
           ) : (
-            <Link to="/login" className="text-neutral-200 rounded-3xl p-3 bg-[#003478]">
+            <Link
+              to="/login"
+              className="text-neutral-200 rounded-3xl p-3 bg-[#003478] hover:bg-[#00c2cd]"
+            >
               تسجيل الدخول
             </Link>
           )}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div id="landing" className="text-neutral-200 landing-image relative">
@@ -8,10 +8,11 @@ const Home = () => {
       <div className="h-dvh container flex items-center justify-center">
         <div className="md:w-3/4 xl:w-1/2 z-10 flex flex-col items-center justify-center h-full">
           <h1 className="lg:text-7xl md:text-right md:text-6xl p-2 text-4xl font-black text-center">
-            منصة <span className="text-[#00c2cd]">عضد </span> للتطوع في العالم العربي
+            منصة <span className="text-[#00c2cd]">عضد </span> للتطوع في العالم
+            العربي
           </h1>
-          <button className="bg-neutral-200 md:ml-auto text-[#003478] font-extrabold text-xl  rounded-md mt-10 py-4 px-16 ">
-            <Link to="vol">كن متطوعًا</Link>
+          <button className="bg-neutral-200 md:ml-auto text-[#003478] hover:bg-[#00c2cd] hover:text-white font-extrabold text-xl  rounded-md mt-10 py-4 px-16 ">
+            <Link to="/opportunities">كن متطوعًا</Link>
           </button>
         </div>
         <div className="md:block lg:w-1/2 md:w-1/4 hidden"></div>
