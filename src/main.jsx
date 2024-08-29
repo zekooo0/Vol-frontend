4a1e0f280f912d620acc4125b221ed77bc89d797
Ahmed Zakaria <ahmedzakria102@gmail.com>
2024-08-29 09:10:39 +0300

    test interworky plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Analytics } from '@vercel/analytics/react';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Analytics } from "@vercel/analytics/react";
+import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
-  </React.StrictMode>,
+    <script
+      src="https://storage.googleapis.com/multisync/interworky/production/interworkyassistant.js"
+      data-api-key="ZTljOGU5NTAtZDU1ZC00ZjI5LThiNWMtZDYyMDQ3NzI4NjJlOmFzc3RfazZCdXFtdHVvMzhzanZyTkUwY1Rjb3hn"
+    ></script>
+  </React.StrictMode>
 );
