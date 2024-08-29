f7032a04bb4d00ac5719a05d17e9417f8eb1555e
Ahmed Zakaria <ahmedzakria102@gmail.com>
2024-08-29 09:19:03 +0300

    fix interworky plugin

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
-    <script
-      src="https://storage.googleapis.com/multisync/interworky/production/interworkyassistant.js"
-      data-api-key="ZTljOGU5NTAtZDU1ZC00ZjI5LThiNWMtZDYyMDQ3NzI4NjJlOmFzc3RfazZCdXFtdHVvMzhzanZyTkUwY1Rjb3hn"
-    ></script>
   </React.StrictMode>
 );
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Container from '../components/Container';
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
 const Layout = () => {
+  useEffect(() => {
+    loadScript(
+      "https://storage.googleapis.com/multisync/interworky/production/interworkyassistant.js",
+      "ZTljOGU5NTAtZDU1ZC00ZjI5LThiNWMtZDYyMDQ3NzI4NjJlOmFzc3RfazZCdXFtdHVvMzhzanZyTkUwY1Rjb3hn"
+    );
+  }, []);
   return (
     <div className="min-h-dvh flex flex-col">
       <Header />
@@ -12,5 +20,11 @@ const Layout = () => {
     </div>
   );
 };
-
+function loadScript(src, dataApiKey) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.setAttribute("data-api-key", dataApiKey);
+  script.async = true;
+  document.body.appendChild(script);
+}
 export default Layout;
